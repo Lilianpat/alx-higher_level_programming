48a3f801c360943427802af9c7be0908cd310879
Lilian Obuzor <lilianobuzorc@gmail.com>
2023-01-03 05:16:55 -0800

    6-print_comb3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+ in range(0, 9):
+     for j in range(1, 10):
+         if j > i:
+             if i != 8:
+                 print("{}{}, ".format(i, j), end="")
+             else:
+                 print("{}{}".format(i, j))
